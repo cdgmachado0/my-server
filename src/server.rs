7c7904f537484e21f29a1b843e67f5264d9b2d2d
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2023-07-11 14:52:35 +0200

    Working on the request

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,8 @@
 use std::io::Read;
+use crate::http::Request;
+use std::convert::TryFrom;
 use std::net::TcpListener;
+
 
 pub struct Server {
     addr: String
@@ -17,15 +20,23 @@ impl Server {
 
         loop {
             match listener.accept() {
-                Ok((mut stream, _)) => {
-                    let a = [1,2,3,3,3,3,4]; 
-                    arr(&a[1..3]);
-                    
-                    // stream.read();
+                Ok((mut stream, _)) => {     
+                    let mut buffer = [0; 1024];  
+
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {},
+                                Err(e) => println!("Failed to parse a request: {}", e)
+                            }
+                        },
+                        Err(e) => println!("Failed to read from connection: {}", e)
+                    }
                 },
                 Err(e) => println!("Connection failed: {}", e)
             }
         }
-
     }
 }
