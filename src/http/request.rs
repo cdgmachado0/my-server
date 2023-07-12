14485b3cc3acbf204aa4b8f0480ca4fee22abd56
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2023-07-13 00:21:49 +0200

    Request ready

--- src/http/request.rs
+++ src/http/request.rs
@@ -4,22 +4,23 @@ use std::error::Error;
 use std::fmt::{Display, Formatter, Result as FmtResult, Debug};
 use std::str;
 use std::str::Utf8Error;
+use super::QueryString;
 
-
-pub struct Request {
-    path: String,
-    query_string: Option<String>,
+#[derive(Debug)]
+pub struct Request<'buf> {
+    path: &'buf str,
+    query_string: Option<QueryString<'buf>>,
     method: Method
-}
+} 
 
-impl TryFrom<&[u8]> for Request {
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from(buf: &'buf [u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buf)?;
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
@@ -29,13 +30,21 @@ impl TryFrom<&[u8]> for Request {
         let method: Method = method.parse()?;
 
         let mut query_string = None;
-        path.find('?');
 
-        unimplemented!()
+        if let Some(i) = path.find('?') {
+            query_string = Some(QueryString::from(&path[i + 1..]));
+            path = &path[..i];
+        }
+
+        Ok(Self {
+            path,
+            query_string,
+            method
+        })
     }
 }
 
-fn get_next_word(request: &str) -> Option<(&str, &str)> {
+fn get_next_word(request: &str) -> Option<(&str, &str)> { 
     for (i, c) in request.chars().enumerate() {
         if c == ' ' || c == '\r' {
             return Some((&request[..i], &request[i + 1..]))
